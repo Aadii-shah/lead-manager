2590db8a71f8ad8516e65c582c0a61853230c976
appDIbetu <dipakshah401@gmail.com>
2021-06-06 09:36:37 +0545

    autocomplete

--- app/src/main/java/com/example/leadmanager/NewLeadActivity.java
+++ app/src/main/java/com/example/leadmanager/NewLeadActivity.java
@@ -3,17 +3,32 @@ package com.example.leadmanager;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+
+import com.example.leadmanager.adapters.AutoCompleteAdapterContact;
+import com.example.leadmanager.models.Contact;
+
+import java.util.List;
+
+import static android.view.View.GONE;
 
 public class NewLeadActivity extends AppCompatActivity {
 
     private EditText editName, editAddress, editEmail, editNumber, editDescription;
     private Spinner spinnerStatus, spinnerSource;
     private Button submitButton;
+    private AutoCompleteTextView contactName;
+    private List<Contact> contactList;
+    private AutoCompleteAdapterContact contactAdapter;
+    private Contact availableContact;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +55,47 @@ public class NewLeadActivity extends AppCompatActivity {
         editEmail = findViewById(R.id.editEmail);
         editNumber = findViewById(R.id.editNumber);
         editDescription = findViewById(R.id.editDescription);
+
+        contactName = findViewById(R.id.contactName);
+
+        contactAdapter = new AutoCompleteAdapterContact(this, contactList);
+
+        contactName.setAdapter(contactAdapter);
+
+        contactName.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                if (!contactName.isPopupShowing()) {
+                    //isSupplierAvailable = false;
+                    //newSupplierLayout.setVisibility(View.VISIBLE);
+                    //addItem.setVisibility(GONE);
+                    //Toast.makeText(requireContext(), "No Item Found", Toast.LENGTH_SHORT).show();
+                } else {
+                    //newSupplierLayout.setVisibility(GONE);
+                    //addItem.setVisibility(GONE);
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        contactName.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                //isSupplierAvailable = true;
+                availableContact = contactAdapter.getItem(i);
+                //newSupplierLayout.setVisibility(GONE);
+                //addItem.setVisibility(View.VISIBLE);
+            }
+        });
 
         submitButton = findViewById(R.id.submitButton);
     }
---
+++ app/src/main/java/com/example/leadmanager/adapters/AutoCompleteAdapterContact.java
@@ -0,0 +1,95 @@
+package com.example.leadmanager.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Filter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import com.example.leadmanager.R;
+import com.example.leadmanager.models.Contact;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.util.Collections.addAll;
+
+public class AutoCompleteAdapterContact extends ArrayAdapter<Contact> {
+    private List<Contact> allSuppliersList;
+    public List<Contact> filteredSuppliersList;
+
+    public AutoCompleteAdapterContact(@NonNull Context context, @NonNull List<Contact> placesList) {
+        super(context, 0, placesList);
+
+        allSuppliersList = new ArrayList<>(placesList);
+    }
+
+    @NonNull
+    @Override
+    public Filter getFilter() {
+        return placeFilter;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        if (convertView == null) {
+            convertView = LayoutInflater.from(getContext()).inflate(
+                    R.layout.contact_item, parent, false
+            );
+        }
+
+        TextView supplierName = convertView.findViewById(R.id.name);
+        TextView supplierPhone = convertView.findViewById(R.id.phone);
+
+        Contact supplier = getItem(position);
+        if (supplier != null) {
+            supplierName.setText(supplier.getName());
+            supplierPhone.setText(supplier.getPhone());
+        }
+
+        return convertView;
+    }
+
+    private Filter placeFilter = new Filter() {
+        @Override
+        protected FilterResults performFiltering(CharSequence constraint) {
+            FilterResults results = new FilterResults();
+
+            filteredSuppliersList = new ArrayList<>();
+
+            if (constraint == null || constraint.length() == 0) {
+                filteredSuppliersList.addAll(allSuppliersList);
+            } else {
+                String filterPattern = constraint.toString().toLowerCase().trim();
+                for (Contact supplier: allSuppliersList) {
+                    if (supplier.getName().toLowerCase().contains(filterPattern)) {
+                        filteredSuppliersList.add(supplier);
+                    }
+                }
+            }
+
+            results.values = filteredSuppliersList;
+            results.count = filteredSuppliersList.size();
+
+            return results;
+        }
+
+        @Override
+        protected void publishResults(CharSequence constraint, FilterResults results) {
+            clear();
+            addAll((List) results.values);
+            notifyDataSetChanged();
+        }
+
+        @Override
+        public CharSequence convertResultToString(Object resultValue) {
+            return ((Contact) resultValue).getName();
+        }
+    };
+}
