36dfc491c8b1b778119d8a789152844f66d8a709
Aadii-shah <badboony0512@gmail.com>
2021-06-12 22:31:51 +0545

    changed Ui of status layout

--- app/src/main/java/com/example/leadmanager/StatusActivity.java
+++ app/src/main/java/com/example/leadmanager/StatusActivity.java
@@ -1,16 +1,17 @@
 package com.example.leadmanager;
 
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.LinearLayout;
+import android.widget.RadioButton;
+
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
-import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.LinearLayout;
-
 import com.example.leadmanager.models.HistoryItem;
-import com.example.leadmanager.models.LeadApp;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
@@ -24,6 +25,8 @@ public class StatusActivity extends AppCompatActivity {
     LinearLayout statusesLayout;
     private FirebaseFirestore db = FirebaseFirestore.getInstance();
     private FirebaseUser user;
+
+    private RadioButton radio_newLead, radio_interested, radio_notInterested, radio_unanswered, radio_busy, radio_converted;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,13 +45,93 @@ public class StatusActivity extends AppCompatActivity {
             }
         });
 
+
+        radio_newLead = findViewById(R.id.radio_newLead);
+        radio_interested = findViewById(R.id.radio_interested);
+        radio_notInterested = findViewById(R.id.radio_notInterested);
+        radio_busy = findViewById(R.id.radio_busy);
+        radio_unanswered = findViewById(R.id.radio_unanswered);
+        radio_converted = findViewById(R.id.radio_converted);
+
+        //retrieving data from SP
+        radio_newLead.setChecked(Update("radio_newLead"));
+        radio_interested.setChecked(Update("radio_interested"));
+        radio_notInterested.setChecked(Update("radio_notInterested"));
+        radio_busy.setChecked(Update("radio_busy"));
+        radio_unanswered.setChecked(Update("radio_unanswered"));
+        radio_converted.setChecked(Update("radio_converted"));
+
+
+        radio_newLead.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean newLead_isChecked) {
+                changeStatus(leadUid, "New Lead");
+                radioSharedPrefs("radio_newLead", newLead_isChecked);
+
+            }
+        });
+
+        radio_interested.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean interested_isChecked) {
+                changeStatus(leadUid, "Interested");
+
+                radioSharedPrefs("radio_interested", interested_isChecked);
+
+            }
+        });
+
+        radio_notInterested.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean notinterested_isChecked) {
+                changeStatus(leadUid, "Not Interested");
+
+                radioSharedPrefs("radio_notInterested", notinterested_isChecked);
+
+            }
+        });
+
+        radio_busy.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean busy_isChecked) {
+                changeStatus(leadUid, "Pending");
+
+                radioSharedPrefs("radio_busy", busy_isChecked);
+
+            }
+        });
+
+        radio_unanswered.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean unAnswered_isChecked) {
+
+                changeStatus(leadUid, "Unanswered");
+
+                radioSharedPrefs("radio_unanswered", unAnswered_isChecked);
+
+            }
+        });
+
+        radio_converted.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean converted_isChecked) {
+
+                changeStatus(leadUid, "Converted");
+
+                radioSharedPrefs("radio_converted", converted_isChecked);
+
+            }
+        });
+
+
         user = FirebaseAuth.getInstance().getCurrentUser();
         statusesLayout = findViewById(R.id.statusesLayout);
 
-        statusesLayout.getChildAt(0).setOnClickListener(new View.OnClickListener() {
+       /* statusesLayout.getChildAt(0).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 changeStatus(leadUid, "New Lead");
+
             }
         });
 
@@ -85,8 +168,23 @@ public class StatusActivity extends AppCompatActivity {
             public void onClick(View view) {
                 changeStatus(leadUid, "Converted");
             }
-        });
+        });*/
     }
+
+
+    private void radioSharedPrefs(String key, boolean value) {
+        SharedPreferences sp = getSharedPreferences("status", MODE_PRIVATE);
+        SharedPreferences.Editor editor = sp.edit();
+        editor.putBoolean(key, value);
+        editor.apply();
+    }
+
+    private boolean Update(String key) {
+        SharedPreferences sp = getSharedPreferences("status", MODE_PRIVATE);
+        return sp.getBoolean(key, false);
+
+    }
+
 
     private void changeStatus(String leadUid, String status) {
         db.collection("cache").document(user.getUid()).collection("leads")
@@ -96,7 +194,7 @@ public class StatusActivity extends AppCompatActivity {
                     @Override
                     public void onSuccess(Void aVoid) {
                         HistoryItem historyItem = new HistoryItem();
-                        historyItem.setDescription("Status changed to: " +  status);
+                        historyItem.setDescription("Status changed to: " + status);
                         historyItem.setDate(Utility.getCurrentTime());
 
                         db.collection("cache").document(user.getUid())
@@ -117,10 +215,12 @@ public class StatusActivity extends AppCompatActivity {
                         //Log.d(TAG, "DocumentSnapshot successfully updated!");
                     }
                 }).addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        //Log.w(TAG, "Error updating document", e);
-                    }
-                });
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                //Log.w(TAG, "Error updating document", e);
+            }
+        });
     }
+
+
 }
