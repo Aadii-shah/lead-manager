80d5be2d8445abba4322ab01abd68679496ab489
Aadii-shah <badboony0512@gmail.com>
2021-06-17 17:27:38 +0545

    added splash screnn

---
+++ app/src/main/java/com/example/leadmanager/SplashActivity.java
@@ -0,0 +1,57 @@
+package com.example.leadmanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        ImageView imageView=(ImageView)findViewById(R.id.imageView); // Declare an imageView to show the animation.
+        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.fade); // Create the animation.
+
+        animation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+
+
+
+
+
+
+        new Handler().postDelayed(new Runnable() {
+
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                Intent i = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 3000);
+    }
+
+    }
