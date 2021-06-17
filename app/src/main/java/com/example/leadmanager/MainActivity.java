6f5828d9a236f57089c436516c135ef3dfa1a8e6
appDIbetu <dipakshah401@gmail.com>
2021-06-17 15:13:33 +0545

    switched bottomnavigationview to bottomappbar

--- app/src/main/java/com/example/leadmanager/MainActivity.java
+++ app/src/main/java/com/example/leadmanager/MainActivity.java
@@ -3,12 +3,15 @@ package com.example.leadmanager;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.content.ContextCompat;
 import androidx.fragment.app.FragmentTransaction;
 
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
@@ -16,6 +19,7 @@ import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.google.android.material.bottomappbar.BottomAppBar;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -31,6 +35,10 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     FirebaseFirestoreSettings settings = new FirebaseFirestoreSettings.Builder()
             .setPersistenceEnabled(true)
             .build();
+
+    private BottomAppBar bottomAppBar;
+    private boolean isHome = true;
+    private FragmentTransaction homeTransaction;
 
 
     @Override
@@ -48,7 +56,79 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
             //  syncData();
         }
 
+        bottomAppBar = findViewById(R.id.bottomAppBar);
+        homeTransaction = getSupportFragmentManager().beginTransaction();
+        homeTransaction.replace(R.id.content, new HomeFragment());
+        homeTransaction.commit();
 
+        bottomAppBar.getMenu().getItem(0).getIcon().setTint(ContextCompat.getColor(getApplicationContext(), R.color.lightBlue));
+
+        bottomAppBar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+
+                Log.v("fjgeryfgyer", "eyfet");
+                FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                MenuItem item1;
+                for(int i = 0; i<5; i++) {
+                    item1 = bottomAppBar.getMenu().getItem(i);
+                    if(item.getItemId() != item1.getItemId()) {
+
+                        switch (item1.getItemId()) {
+                            case R.id.home:
+                                item1.setIcon(ContextCompat.getDrawable(getApplicationContext(), R.drawable.ic_home));
+                                break;
+
+
+                            case R.id.contact:
+                                item1.setIcon(ContextCompat.getDrawable(getApplicationContext(), R.drawable.ic_contact));
+                                break;
+
+
+                            case R.id.task:
+                                item1.setIcon(ContextCompat.getDrawable(getApplicationContext(), R.drawable.ic_task));
+                                break;
+
+
+                            case R.id.schedule:
+                                item1.setIcon(ContextCompat.getDrawable(getApplicationContext(), R.drawable.ic_calendar));
+                                break;
+                        }
+
+                        item1.getIcon().setTint(ContextCompat.getColor(getApplicationContext(), R.color.lightGrey));
+                    }
+                }
+
+                item.getIcon().setTint(ContextCompat.getColor(getApplicationContext(), R.color.lightBlue));
+
+                switch (item.getItemId()) {
+                    case R.id.home:
+                        transaction.replace(R.id.content, new HomeFragment());
+                        isHome = true;
+                        break;
+
+
+                    case R.id.contact:
+                        transaction.replace(R.id.content, new ContactFragment());
+                        isHome = false;
+                        break;
+
+
+                    case R.id.task:
+                        transaction.replace(R.id.content, new TaskFragment());
+                        isHome = false;
+                        break;
+
+
+                    case R.id.schedule:
+                        transaction.replace(R.id.content, new ScheduleFragment());
+                        isHome = false;
+                        break;
+                }
+                transaction.commit();
+                return true;
+            }
+        });
 //        spinner = findViewById(R.id.daySpinner);
 //        ArrayAdapter adapter = ArrayAdapter.createFromResource(this,
 //                R.array.day_array, R.layout.color_spinner_layout);
@@ -58,10 +138,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 //        spinner.setAdapter(adapter);
 //       // spinner.setOnItemSelectedListener(this);
 
-        bottomNavigationView = findViewById(R.id.bottomNavigationView);
-        FragmentTransaction homeTransaction = getSupportFragmentManager().beginTransaction();
-        homeTransaction.replace(R.id.content, new HomeFragment());
-        homeTransaction.commit();
+        /*bottomNavigationView = findViewById(R.id.bottomNavigationView);
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
@@ -91,7 +168,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
                 transaction.commit();
                 return true;
             }
-        });
+        });*/
     }
 
     @Override
@@ -119,7 +196,7 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     @Override
     public void onBackPressed() {
 
-        if (bottomNavigationView.getSelectedItemId() == R.id.home) {
+        if (isHome) {
 
             new AlertDialog.Builder(this)
                     .setIcon(android.R.drawable.ic_dialog_alert)
@@ -134,6 +211,13 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
                     })
                     .setNegativeButton("No", null)
                     .show();
-        } else bottomNavigationView.setSelectedItemId(R.id.home);
+        } else {
+            getSupportFragmentManager().beginTransaction().replace(R.id.content, new HomeFragment()).commit();
+            bottomAppBar.getMenu().getItem(0).getIcon().setTint(ContextCompat.getColor(getApplicationContext(), R.color.lightBlue));
+            for(int i = 1; i<5; i++) {
+                bottomAppBar.getMenu().getItem(i).getIcon().setTint(ContextCompat.getColor(getApplicationContext(), R.color.lightGrey));
+            }
+        }
     }
+
 }
